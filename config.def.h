b60579b69364fe2918127412e4f8ddad57a35ad5
The-Repo-Club <wayne6324@gmail.com>
2023-08-24 23:56:06 +0100

    fix formatting
    Signed-off-by: The-Repo-Club <wayne6324@gmail.com>

--- config.def.h
+++ config.def.h
@@ -1,24 +1,27 @@
 /* See LICENSE file for copyright and license details. */
 
 /* alt-tab configuration */
-static const unsigned int tabModKey       = 0x40;    /* if this key is hold the alt-tab functionality stays acitve. This key must be the same as key that is used to active functin altTabStart `*/
-static const unsigned int tabCycleKey     = 0x17;    /* if this key is hit the alt-tab program moves one position forward in clients stack. This key must be the same as key that is used to active functin altTabStart */
-static const unsigned int tabPosY         = 1;    /* tab position on Y axis, 0 = bottom, 1 = center, 2 = top */
-static const unsigned int tabPosX         = 1;    /* tab position on X axis, 0 = left, 1 = center, 2 = right */
-static const unsigned int maxWTab         = 600;    /* tab menu width */
-static const unsigned int maxHTab         = 200;    /* tab menu height */
+static const unsigned int tabModKey             = 0x40;    /* if this key is hold the alt-tab functionality stays acitve. This key must be the same as key that is used to active functin altTabStart `*/
+static const unsigned int tabCycleKey           = 0x17;    /* if this key is hit the alt-tab program moves one position forward in clients stack. This key must be the same as key that is used to active functin altTabStart */
+static const unsigned int tabPosY               = 1;    /* tab position on Y axis, 0 = bottom, 1 = center, 2 = top */
+static const unsigned int tabPosX               = 1;    /* tab position on X axis, 0 = left, 1 = center, 2 = right */
+static const unsigned int maxWTab               = 600;    /* tab menu width */
+static const unsigned int maxHTab               = 200;    /* tab menu height */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft  = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 2;        /* 2 is the default spacing around the bar's font */
+static const unsigned int borderpx              = 1;        /* border pixel of windows */
+static const unsigned int snap                  = 32;       /* snap pixel */
+static const unsigned int systraypinning        = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft         = 0;   /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing        = 2;   /* systray spacing */
+static const int systraypinningfailfirst        = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray                    = 1;        /* 0 means no systray */
+static const int showbar                        = 1;        /* 0 means no bar */
+static const int topbar                         = 1;        /* 0 means bottom bar */
+static const int user_bh                        = 2;        /* 2 is the default spacing around the bar's font */
+
+
+
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
